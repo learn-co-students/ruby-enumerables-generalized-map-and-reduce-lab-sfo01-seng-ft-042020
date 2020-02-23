c61897f0dca504b451d3ca632dda3d2f3d1d256d
Connor Blakeney <connorblakeney@yahoo.com>
2020-02-23 23:21:59 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(s)
+  new = []
+  i = 0
+  while i < s.length do
+    new.push(yield(s[i]))
+    i += 1
+  end
+  new
+end
+
+def reduce(s, sp=nil)
+  if sp
+    accum = sp
+    i = 0
+  else
+    accum = s[0]
+    i = 1
+  end
+  while i < s.length do
+    accum = yield(accum, s[i])
+    i += 1
+  end
+  accum
+end
